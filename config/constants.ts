45eda661e07386489c4f298ade57375d7744ebad
sandesh sidanale <sandeshsidanale@gmail.com>
2024-11-21 22:57:32 +0530

    get public url from s3 with expiry time

--- config/constants.ts
+++ config/constants.ts
@@ -2,5 +2,8 @@
 export const PORT = process.env.PORT || 3000
 export const DB_TYPE = process.env.DB_TYPE || 'sqlite'
 export const DATABASE = process.env.DATABASE || '../db.sqlite'
+
 export const AWS_REGION = process.env.AWS_REGION || 'us-east-1'
 export const S3_BUCKET = process.env.S3_BUCKET
+
+export const URL_EXPIRY_IN_HOURS = process.env.URL_EXPIRY_IN_HOURS || '6'
